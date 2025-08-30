9711ce6a2d7dcf1910cd0e7379fd062c031dfa56
m4ntonio <m4ntonio@hotmail.com>
2025-08-29 23:01:06 -0300

    tipos

---
+++ tipos.py
@@ -0,0 +1,21 @@
+var = 1
+var2 = 10.4
+var = 15.2
+
+# comentar o meu código
+nome = "Mario" + "10"
+booleano = True
+booleano = False
+Lista = ["macarrão", "pão", "arroz", "molho"]
+lista2 = [1,2,3,4]
+
+'''
+pode escrever aqui
+pode escrever aqui
+pode escrever aqui
+pode escrever aqui
+'''
+
+resultado = var + var2
+print(resultado)
+print(nome)
