35389ed34e1576ec7a914df84d53ed350094fcf8
m4ntonio <m4ntonio@hotmail.com>
2025-09-05 22:40:19 -0300

    update

--- aula1/ex4.py
+++ aula1/ex4.py
@@ -24,3 +24,7 @@ velocidade_media = 60  # km/h
 
 tempo = distancia / velocidade_media
 print("4. Tempo da viagem:", tempo, "horas")
+
+fahrenheit = float(input("Digite a temperatura em Fahrenheit: "))
+celsius = (fahrenheit - 32) * 5/9
+print(f"A Temperatura em Celsius é: {celsius:.2f}")
