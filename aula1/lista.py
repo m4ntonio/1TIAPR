687f0e5e8895732d91c87c28a7b420500e43d3ed
m4ntonio <m4ntonio@hotmail.com>
2025-09-12 22:43:03 -0300

    ultima

---
+++ aula1/lista.py
@@ -0,0 +1,11 @@
+lista_mercado = []
+lista_mercado.append("banana")
+lista_mercado.append("maçã")
+lista_mercado.append("laranja")
+lista_mercado.insert(1, "uva")
+lista_mercado[0] = "abacaxi"
+lista_mercado.remove("maçã")
+
+
+
+print(lista_mercado)
---
+++ aula1/lista2.py
@@ -0,0 +1,24 @@
+numeros = [1, 2, 3, 4, 5]
+soma = sum(numeros)
+print("A soma dos números é:", soma)
+
+string = ["Python", "linguagem", "programação"]
+print(", ".join(string))
+
+numeros = [2, 4, 6, 8, 73]
+maior = max(numeros)
+print("O maior número é:", maior)
+
+nomes = ["Ana", "Bruno", "Carlos", "Diana", "Eduardo"]
+
+nome = "Mario"
+
+if nome in nomes:
+    print(f"{nome} está presente na lista.")
+else:
+    print(f"{nome} não está presente na lista.")
+
+numbers = [10, 20, 30, 40, 50]
+average = sum(numbers) / len(numbers)
+
+print("A média dos números é:", average)
