a6ad8f9e936c121eb3aa7c2273cd68458589ace5
m4ntonio <m4ntonio@hotmail.com>
2025-09-16 22:39:39 -0300

    update

---
+++ aula2/ex5.py
@@ -0,0 +1,5 @@
+produto = 1
+for numero in range(1, 5):
+    produto *= numero
+
+print("O produto dos números de 1 a 5 é:", produto)
--- aula2/lista2.py
+++ aula2/lista2.py
@@ -1,16 +1,27 @@
+#1
 numeros = [1, 2, 3, 4, 5]
 soma = sum(numeros)
 print("A soma dos números é:", soma)
 
+resultado = 0
+for numero in numeros:
+    resultado += numero
+print("A soma dos números é:", resultado)
+
+
+#2
 string = ["Python", "linguagem", "programação"]
 print(", ".join(string))
 
+
+#3
 numeros = [2, 4, 6, 8, 73]
 maior = max(numeros)
 print("O maior número é:", maior)
 
-nomes = ["Ana", "Bruno", "Carlos", "Diana", "Eduardo"]
 
+#4
+nomes = ["Ana", "Bruno", "Carlos", "Diana", "Eduardo"]
 nome = "Mario"
 
 if nome in nomes:
@@ -18,6 +29,8 @@ if nome in nomes:
 else:
     print(f"{nome} não está presente na lista.")
 
+
+#5
 numbers = [10, 20, 30, 40, 50]
 average = sum(numbers) / len(numbers)
 
