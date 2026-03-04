f41e4c0556023373659262ce36ead7c223b2a3d4
m4ntonio <m4ntonio@hotmail.com>
2026-03-03 22:02:44 -0300

    update

---
+++ 2026/aula11/leitor.py
@@ -0,0 +1,10 @@
+try:
+    with open(caminho_arquivo, 'rb') as arquivo:
+        conteudo = arquivo.read()
+        hex_dados = conteudo.hex().upper()
+
+        for i in range(0, len(hex_dados), 2):
+            print(hex_dados[i:i+2], end='')
+
+except Exception as e:
+    print(f"Erro ao ler o arquivo: {e}")
